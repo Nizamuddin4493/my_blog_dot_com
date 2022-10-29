a6762744858664d744b591f737c4bbc7250623e2
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-10-29 23:26:59 +0430

    readme file is updated and the column attribute type changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_29_174943) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_29_182457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_174943) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "photo"
+    t.string "photo"
     t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
